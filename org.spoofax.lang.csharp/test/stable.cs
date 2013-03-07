e36ce4617a5038bcf00df5c8f9087fdb6b0980d7
guwac <g.h.wachsmuth@tudelft.nl>
2013-03-07 11:00:08 +0100

    example for stable URIs

---
+++ org.spoofax.lang.csharp/test/stable.cs
@@ -0,0 +1,28 @@
+namespace StableURIs {
+  
+  class Foo {
+
+    void bar() {
+   
+      int i;
+      int j;
+         
+      if (true) {
+      
+        int i = i;
+        int j = i;
+      
+      } else {
+      
+        int i = i;
+        int j = i;
+      }
+      
+      int k = i;
+    }
+  }
+  
+  class Foo {}
+  
+  class Foo {}
+}
